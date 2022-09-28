20fdbc24498e96d359dc4c7fdecceb6eab36603c
subasskhatrii <subash@teksewa.com>
2022-09-28 23:54:41 +0930

    - Add method for user selection - Add method for computer selection

--- the_rps_game.py
+++ the_rps_game.py
@@ -1,1 +1,20 @@
-import unittest
+import random
+from enum import IntEnum
+
+class Action(IntEnum):
+    Rock = 0
+    Paper = 1
+    Scissors = 2
+
+class TheRPSGame():
+    def get_user_selection(self):
+        user_input = input("Enter a choice (rock[0], paper[1], scissors[2]): ")
+        selection = int(user_input)
+        action = Action(selection)
+        return action
+
+    def get_computer_selection(self):
+        selection = random.randint(0, len(Action) - 1)
+        action = Action(selection)
+        return action
+
