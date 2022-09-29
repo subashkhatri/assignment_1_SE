938a66868ca4e671f5abab3a0dce5ee6e35b71ec
subasskhatrii <subash@teksewa.com>
2022-09-29 10:31:59 +0930

    - Add method for checking the wining player

--- the_rps_game.py
+++ the_rps_game.py
@@ -18,3 +18,30 @@ class TheRPSGame():
         action = Action(selection)
         return action
 
+    def check_winning_player(self, user_action = 0, computer_action = 0):
+        user_count, computer_count = 0, 0
+        if user_action == computer_action:
+            print(f"Both players selected {user_action}. It's a tie!")
+            user_count += 0
+            computer_count += 0
+        elif user_action == Action.Rock:
+            if computer_action == Action.Scissors:
+                print("Rock smashes scissors! You win!")
+                user_count += 1
+            else:
+                print("Paper covers rock! You lose.")
+                computer_count += 1
+        elif user_action == Action.Paper:
+            if computer_action == Action.Rock:
+                print("Paper covers rock! You win!")
+                user_count += 1
+            else:
+                print("Scissors cuts paper! You lose.")
+                computer_count += 1
+        elif user_action == Action.Scissors:
+            if computer_action == Action.Paper:
+                print("Scissors cuts paper! You win!")
+                user_count += 1
+            else:
+                print("Rock smashes scissors! You lose.")
+                computer_count += 1
