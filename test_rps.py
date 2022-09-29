53f05359088b3f42b08e1ab1ecaf112300f014bd
subasskhatrii <subash@teksewa.com>
2022-09-29 10:31:41 +0930

    - Add test case for wining player

--- test_rps.py
+++ test_rps.py
@@ -1,7 +1,16 @@
 from logging import error
 from pdb import set_trace
+import random
 import unittest
+from unittest.mock import patch
+
+from enum import IntEnum
 from the_rps_game import TheRPSGame
+
+class Action(IntEnum):
+    Rock = 0
+    Paper = 1
+    Scissors = 2
 
 class  UnitTestRPSGame(unittest.TestCase):
     def test_computer_selection(self):
@@ -11,31 +20,45 @@ class  UnitTestRPSGame(unittest.TestCase):
             results.append(the_rps_game.get_computer_selection())
 
         new_result = len(set(results))
-        # set_trace()
+
         if new_result > 1:
-            print("Pass")
+            print("Test passed!")
         else:
-            print("Fail")
-            self.fail("shouldn't happen")
+            print("Test has failed!")
+            self.fail("Fail")
 
     def test_user_input(self):
         the_rps_game = TheRPSGame()
         results = the_rps_game.get_user_selection()
 
-        if results == 0:
-            print("Pass")
+        if results == Action.Rock:
+            print("Test passed!")
             pass
-        elif results == 1:
-            print("Pass")
+        elif results == Action.Paper:
+            print("Test passed!")
             pass
-        elif results == 2:
-            print("Pass")
+        elif results == Action.Scissors:
+            print("Test passed!")
             pass
         else:
-            print("Fail")
-            self.fail("shouldn't happen")
+            print("Test has failed!")
+            self.fail("Fail")
 
+    def test_winning_player(self):
+        user_selection = random.randint(0,3)
+        computer_selection = random.randint(0,3)
+        the_rps_game = TheRPSGame()
+        the_rps_game.check_winning_player(user_selection, computer_selection)
 
+        if user_selection > 3 or user_selection < 0:
+            print("Test case has failed!")
+            self.fail("Failed")
+        elif computer_selection > 3 or computer_selection < 0:
+            print("Test case has failed!")
+            self.fail("Failed")
+        else:
+            print("Test has passed!")
+            pass
 
     if __name__ == '__main__':
         unittest.main()
